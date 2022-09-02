ee5fb7b651776897f4b21cc81d856398f3d78dc7
tingyusha <916878440@qq.com>
2022-09-02 20:39:19 +0800

    添加徽章

---
+++ SwiftUIDemo/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUIDemo
+//
+//  Created by sam on 2022/9/2.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
--- SwiftUIDemo/Views/HexagonParameters.swift
+++
@@ -1,8 +0,0 @@
-//
-//  HexagonParameters.swift
-//  SwiftUIDemo
-//
-//  Created by sam on 2022/9/2.
-//
-
-import Foundation
